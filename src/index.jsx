5fc056b511196e7a633ba212da5bd5c4daa7a148
Hana Sedlaříková <hana.sedlarikova@gmail.com>
2023-03-20 09:54:10 +0100

    Vloženi barev

--- src/index.jsx
+++ src/index.jsx
@@ -3,23 +3,53 @@ import { createRoot } from 'react-dom/client';
 import './style.css';
 
 const App = () => {
+
+
+
+
   return (
-    <div className="container">
-      <header>
-        <div className="logo" />
-        <h1>React webová aplikace</h1>
-      </header>
-      <main>
-        <p>
-          Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí
-          <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>
-          .
-        </p>
-      </main>
-      <footer>
-        <p>Czechitas, Digitální akademie: Web</p>
-      </footer>
-    </div>
+    <>
+
+ <div class="color-panel">
+		<h1>Mixér barev</h1>
+		<div class="sliders">
+			<label for="red">Červená</label>
+			<input
+        type="range"
+        class="slider slider--red"
+        id="redSlider"
+        min="0"
+        max="255"
+        value="0"
+      />
+
+			<label for="green">Zelená</label>
+			<input
+        type="range"
+        class="slider slider--green"
+        id="greenSlider"
+        min="0"
+        max="255"
+        value="0"
+      />
+
+			<label for="blue">Modrá</label>
+			<input
+        type="range"
+        class="slider slider--blue"
+        id="blueSlider"
+        min="0"
+        max="255"
+        value="0"
+      />
+		</div>
+		<div class="color-box" id="color-box"></div>
+  </div>
+
+
+    </>
+
+   
   );
 };
 
